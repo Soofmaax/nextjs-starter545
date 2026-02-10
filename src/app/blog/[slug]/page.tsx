dc0ca254ca7fa05053f8a20f0dc895272ac5a002
Soofmax <salwaessaf@gmail.com>
2026-02-10 18:05:02 +0000

    refactor: update BlogPostPage props to use App Router params (Promise<{ slug }>)
    Co-authored-by: Cosine <agent@cosine.sh>

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,12 @@
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { PortableText } from "@portabletext/react";
-import type { PageProps } from "next";
 
 import { getSiteSettings, DEFAULT_SITE_SETTINGS, getPostBySlug, getRelatedPosts, type SanityPost } from "../../../lib/sanity.client";
 
-type BlogPostPageProps = PageProps<{ slug: string }>;
+type BlogPostPageProps = {
+  params: Promise<{ slug: string }>;
+};
 
 export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
